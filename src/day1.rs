a76cf971bbb694ed6c6348b5d986c58077dabd34
XavierT <sseingalt@gmail.com>
2018-12-02 18:05:02 +0100

    run rustfmt

--- src/day1.rs
+++ src/day1.rs
@@ -1,20 +1,19 @@
 
 
-pub fn run(){
+pub fn run() {
     println!("day1 is running!");
 
     let content = include_str!("../input/day1.txt");
 
-    let vec:Vec<_> = content.lines().collect();
+    let vec: Vec<_> = content.lines().collect();
 
-    let mut ivec:Vec<i32> = Vec::new();
+    let mut ivec: Vec<i32> = Vec::new();
 
-    for s in vec.iter()
-    {
-        ivec.push( s.parse().unwrap());
+    for s in vec.iter() {
+        ivec.push(s.parse().unwrap());
     }
 
-    let sum:i32 = ivec.iter().sum();
+    let sum: i32 = ivec.iter().sum();
 
-    println!(" The sum of the vector is {}",sum);
+    println!(" The sum of the vector is {}", sum);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use std::env;
 fn main() {
 
     let args: Vec<_> = env::args().collect();
-        if args.len() > 1 {
+    if args.len() > 1 {
 
-            match args[1].as_str()
-            {
-                "day1" => day1::run(),
-                _ => println!("Unknown day"),
-            }
+        match args[1].as_str() {
+            "day1" => day1::run(),
+            _ => println!("Unknown day"),
         }
+    }
 
 
 
