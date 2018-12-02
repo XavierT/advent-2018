f02e8e69dc6af004921e1423ba89176764fd3e05
XavierT <sseingalt@gmail.com>
2018-12-02 17:55:52 +0100

    first star!

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,20 @@
 
+
 pub fn run(){
     println!("day1 is running!");
+
+    let content = include_str!("../input/day1.txt");
+
+    let vec:Vec<_> = content.lines().collect();
+
+    let mut ivec:Vec<i32> = Vec::new();
+
+    for s in vec.iter()
+    {
+        ivec.push( s.parse().unwrap());
+    }
+
+    let sum:i32 = ivec.iter().sum();
+
+    println!(" The sum of the vector is {}",sum);
 }
