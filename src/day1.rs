f8af68fc4f62f01e3afd02f96a0e7fe00eaff1b5
XavierT <sseingalt@gmail.com>
2018-12-05 20:56:15 +0100

    add some comment

--- src/day1.rs
+++ src/day1.rs
@@ -15,5 +15,13 @@ pub fn run() {
 
     let sum: i32 = ivec.iter().sum();
 
+    // solution for Part 1
     println!(" The sum of the vector is {}", sum);
+
+
+    // Part 2
+    // Do the sum and store the intermediate result in a hashmap, with the partial sum as index
+    // 1 if the sum happened once
+    // 2 if the sum happened twice
+    // Stopped when the first 2 is reached
 }
